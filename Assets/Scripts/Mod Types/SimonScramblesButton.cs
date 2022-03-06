78296ea99a1e05722bbdba091efa34f2bd91aed6
Danny7007 <dannypignuola@gmail.com>
2022-03-06 10:48:03 -0500

    Add Simon Scrambles

---
+++ Assets/Scripts/Mod Types/SimonScramblesButton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonScramblesButton : SimonButton {
+    public override SimonModType type
+    { get { return SimonModType.Simon_Scrambles; } }
+
+    protected override Dictionary<SimonColor, Color> matColors
+    { get { return new Dictionary<SimonColor, Color>()
+            {
+                { SimonColor.Red, new Color32(0xFF, 0x00, 0x00, 0xFF)},
+                { SimonColor.Green, new Color32(0x68, 0xFF, 0x00, 0xFF)},
+                { SimonColor.Blue, new Color32(0x09, 0x0B, 0xFF, 0xFF)},
+                { SimonColor.Yellow, new Color32(0xFF, 0xF8, 0x00, 0xFF)}
+            }; } 
+    }
+
+    public override float interactionPunch
+    { get { return 0; } }
+
+    protected override float flashTime
+    { get { return 0.9f; } }
+
+    public override IEnumerable<Pair<SimonColor, SpecialEventType?>> GetSolution(Flash thisFlash)
+    {
+        return PopulateWithNull(CalculateSolution(thisFlash));
+    }
+    IEnumerable<SimonColor> CalculateSolution(Flash thisFlash)
+    {
+        Log("Indexing into color table with color {0} and positions {1} {2}.", this.color, thisFlash.flashPosition, thisFlash.flashPosition + 5);
+        yield return table[this.color][thisFlash.positionInSequence];
+        yield return table[this.color][thisFlash.positionInSequence + 5];
+
+    }
+
+    public override string GetFlashSound()
+    {
+        return null;
+    }
+
+    private static readonly Dictionary<SimonColor, SimonColor[]> table = new Dictionary<SimonColor, SimonColor[]>()
+    {
+        { SimonColor.Blue, new[] { SimonColor.Yellow, SimonColor.Green, SimonColor.Red, SimonColor.Red, SimonColor.Red, SimonColor.Blue, SimonColor.Yellow, SimonColor.Yellow, SimonColor.Red, SimonColor.Green, } },
+        { SimonColor.Yellow, new[] { SimonColor.Green, SimonColor.Blue, SimonColor.Green, SimonColor.Yellow, SimonColor.Blue, SimonColor.Yellow, SimonColor.Green, SimonColor.Blue, SimonColor.Yellow, SimonColor.Red} },
+        { SimonColor.Red, new[] { SimonColor.Blue, SimonColor.Yellow, SimonColor.Yellow, SimonColor.Green, SimonColor.Green, SimonColor.Red, SimonColor.Blue, SimonColor.Green, SimonColor.Blue, SimonColor.Yellow } },
+        { SimonColor.Green, new[] { SimonColor.Red, SimonColor.Red, SimonColor.Blue, SimonColor.Blue, SimonColor.Yellow, SimonColor.Green, SimonColor.Red, SimonColor.Red, SimonColor.Green, SimonColor.Blue } }
+    };
+}
